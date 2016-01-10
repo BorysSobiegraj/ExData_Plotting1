7f0b1164ea12ced1b044bbb74264ce5910e3111e
BorysSobiegraj <borys.sobiegraj@gmail.com>
2016-01-10 16:02:07 +0100

    Signed-off-by: BorysSobiegraj <borys.sobiegraj@gmail.com>

--- project/plot2.R
+++ project/plot2.R
@@ -1,7 +1,10 @@
 
 powcons <- read.table("household_power_consumption.txt", sep = ";", dec = '.', header = TRUE, na.strings = "?")
 
-SubsetHouseholdPow <- subset(powcons,powcons$Date == c("1/2/2007","2/2/2007") )
+#install.packages('sqldf')
+library(sqldf)
+
+SubsetHouseholdPow <- sqldf("select * from powcons where Date in ('1/2/2007','2/2/2007')")
 SubsetHouseholdPow["DateTimeVar"] <- as.POSIXct(strptime(paste(SubsetHouseholdPow$Date, SubsetHouseholdPow$Time,sep = " "),"%d/%m/%Y %H:%M:%S"))
 
 
--- project/plot4.R
+++ project/plot4.R
@@ -1,8 +1,10 @@
 
-setwd("C:\\Users\\bsobiegraj001\\Desktop\\Data Scientist track\\Data Exploration")
 powcons <- read.table("household_power_consumption.txt", sep = ";", dec = '.', header = TRUE, na.strings = "?")
 
-SubsetHouseholdPow <- subset(powcons,powcons$Date == c("1/2/2007","2/2/2007") )
+#install.packages('sqldf')
+library(sqldf)
+
+SubsetHouseholdPow <- sqldf("select * from powcons where Date in ('1/2/2007','2/2/2007')")
 SubsetHouseholdPow["DateTimeVar"] <- as.POSIXct(strptime(paste(SubsetHouseholdPow$Date, SubsetHouseholdPow$Time,sep = " "),"%d/%m/%Y %H:%M:%S"))
 
 ########################cREATING MULTI PLOT PLOT ################
@@ -51,3 +53,4 @@ plot(SubsetHouseholdPow$DateTimeVar,SubsetHouseholdPow$Global_reactive_power
 dev.off() 
 
 
+?text
